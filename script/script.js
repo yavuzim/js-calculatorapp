413df5d8c0361a6341cc79a2b8f81f64f16a4ed6
90539 <yavuz.imer133@gmail.com>
2021-09-28 10:17:59 +0300

    calculator

---
+++ script/script.js
@@ -0,0 +1,88 @@
+let arr = []
+let processValue = [], conclusion, operator, setValue
+const add = (e) => {
+    let sayi = e.value
+    arr.push(sayi)
+    txt()
+}
+const txt_delete = (e) => {
+    arr.splice(arr.length - 1, 1)
+    txt()
+}
+const full_delete = (e) => {
+    arr.splice(0)
+    txt()
+}
+const txt = () => {
+    document.querySelector(".txt").value = null
+    arr.forEach(e => document.querySelector(".txt").value += e)
+}
+
+const process = (e) => {
+    if (arr.length > 0) {
+        setValue = Number(arr.splice(',').join(""))
+        arr.splice(0)
+        processValue.push(setValue)
+    }
+    txt()
+    if (e.value === "+") {
+        if (processValue.length === 1) conclusion = processValue[0]
+        else {
+            processValue.forEach((e, i) => {
+                if (i > 0) {
+                    conclusion += e
+                    processValue.splice(i, 1)
+                }
+            })
+        }
+        operator = "+"
+    }
+    if (e.value === "-") {
+        if (processValue.length === 1) conclusion = processValue[0]
+        else {
+            processValue.forEach((e, i) => {
+                if (i > 0) {
+                    conclusion -= e
+                    processValue.splice(i, 1)
+                }
+            })
+        }
+        operator = "-"
+    }
+    if (e.value === "x") {
+        if (processValue.length === 1) conclusion = processValue[0]
+        else {
+            processValue.forEach((e, i) => {
+                if (i > 0) {
+                    conclusion *= e
+                    processValue.splice(i, 1)
+                }
+            })
+        }
+        operator = "x"
+    }
+    if (e.value === "/") {
+        if (processValue.length === 1) conclusion = processValue[0]
+        else {
+            processValue.forEach((e, i) => {
+                if (i > 0) {
+                    conclusion /= e
+                    processValue.splice(i, 1)
+                }
+            })
+        }
+        operator = "/"
+    }
+    if (e.value === "=") {
+        console.log("=====");
+        if (operator === "+") conclusion = processValue[0] + setValue
+        if (operator === "-") conclusion = processValue[0] - setValue
+        if (operator === "x") conclusion = processValue[0] * setValue
+        if (operator === "/") conclusion = processValue[0] / setValue
+        operator = ""
+    }
+    processValue.splice(0)
+    processValue.push(conclusion)
+    document.querySelector(".txt").value = conclusion
+}
+
